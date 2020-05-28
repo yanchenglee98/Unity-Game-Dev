320274cb29d2fe4c3a14629d3e3ae705b4baa0f6
yanchenglee98 <yanchenglee@gmail.com>
2020-05-28 21:17:46 +0800

    add hook

---
+++ Jungle Explorer/Assets/Scripts/throwhook.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class throwhook : MonoBehaviour
+{
+
+
+    public GameObject hook;
+
+
+    public bool ropeActive;
+
+    GameObject curHook;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 destiny = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (!ropeActive)
+            {
+
+                RaycastHit2D hit = Physics2D.Raycast(destiny, Vector2.zero);
+
+                if (hit != null && hit.collider != null)
+                {
+                    curHook = (GameObject)Instantiate(hook, transform.position, Quaternion.identity);
+
+                    curHook.GetComponent<RopeScript>().destiny = destiny;
+
+                    ropeActive = true;
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            //delete rope
+
+            Destroy(curHook);
+
+            ropeActive = false;
+        }
+
+
+    }
+}
